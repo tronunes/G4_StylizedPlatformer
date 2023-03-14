f04bdac7635312a0e3a7637d933766d17c0253f8
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-03-14 18:21:33 +0200

    Added a basic 3rd person camera control
    A very basic though. Is very bad currently.

--- IndianaFrog/Assets/Scripts/Player/PlayerCameraController.cs
+++ IndianaFrog/Assets/Scripts/Player/PlayerCameraController.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class PlayerCameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Technical")]
+    [SerializeField] private Transform cameraPivotHorizontal;
+    [SerializeField] private Transform cameraPivotVertical;
+
+    [Header("Settings")]
+    private float horizontalRotateSpeedSetting = 3f;
+    private float verticalRotateSpeedSetting = 3f;
+    private float horizontalDeadZoneSetting = 50f;
+    private float verticalDeadZoneSetting = 30f;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Mouse position's offset from the center of the screen
+        float mouseHorizontalOffset = Input.mousePosition.x - Screen.width / 2f;
+        float mouseVerticalOffset = Input.mousePosition.y - Screen.height / 2f;
+
+        // Adjust the camera to turn faster the more there is offset
+        float directionSign = mouseHorizontalOffset > 0 ? 1f : -1f;
+        float turnSpeedHorizontal = horizontalRotateSpeedSetting * ((mouseHorizontalOffset - directionSign * horizontalDeadZoneSetting) / Screen.width);
+        directionSign = mouseVerticalOffset > 0 ? 1f : -1f;
+        float turnSpeedVertical = verticalRotateSpeedSetting * ((mouseVerticalOffset - directionSign * verticalDeadZoneSetting) / Screen.height);
+
+        // Horizontal rotation
+        if (mouseHorizontalOffset < -horizontalDeadZoneSetting)
+        {
+            cameraPivotHorizontal.Rotate(0f, turnSpeedHorizontal, 0f);
+        }
+        if (mouseHorizontalOffset > horizontalDeadZoneSetting)
+        {
+            cameraPivotHorizontal.Rotate(0f, turnSpeedHorizontal, 0f);
+        }
+
+        // Vertical rotation
+        if (mouseVerticalOffset < -verticalDeadZoneSetting)
+        {
+            cameraPivotVertical.Rotate(turnSpeedVertical, 0f, 0f);
+        }
+        if (mouseVerticalOffset > verticalDeadZoneSetting)
+        {
+            cameraPivotVertical.Rotate(turnSpeedVertical, 0f, 0f);
+        }
     }
 }
