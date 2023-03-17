cea240bd517c948b2dc158f7c4b825f2161a687e
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-03-17 18:28:04 +0200

    Copy the Platform's velocity and rotation to the Character Controller

--- IndianaFrog/Assets/Scripts/Player/GroundCheck.cs
+++ IndianaFrog/Assets/Scripts/Player/GroundCheck.cs
@@ -9,17 +9,31 @@ public class GroundCheck : MonoBehaviour
 
     void OnTriggerEnter(Collider collider)
     {
+        // Grounded
         if (!collider.gameObject.CompareTag("Player"))
         {
             playerMovement.SetGroundedState(true);
+        }
+
+        // Parenting to Platform
+        if (collider.gameObject.CompareTag("Platform"))
+        {
+            playerMovement.ParentToPlatform(collider.transform);
         }
     }
 
     void OnTriggerExit(Collider collider)
     {
+        // Ungrounded
         if (!collider.gameObject.CompareTag("Player"))
         {
             playerMovement.SetGroundedState(false);
+        }
+
+        // Unparent from Platform
+        if (collider.gameObject.CompareTag("Platform"))
+        {
+            playerMovement.ClearParent();
         }
     }
 }
