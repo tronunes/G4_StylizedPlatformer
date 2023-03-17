e5de32ff17e3cec7b56405f37cd5b85096663b1f
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-03-17 15:47:53 +0200

    Implemented a basic rotating platform

--- IndianaFrog/Assets/Scripts/Platforms/RotatingPlatform.cs
+++ IndianaFrog/Assets/Scripts/Platforms/RotatingPlatform.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class RotatingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform platform;
+    private Quaternion meshOrignalRotation;
+    [SerializeField] private Transform rotationPivot;
+    [SerializeField] bool onlyOrbitWithoutRotation = false; // If true, the platform mesh won't rotate when orbiting the pivot
+    [SerializeField] bool counterClockwise = false; // Otherwise orbiting is clockwise viewed from above
+    [SerializeField] private float orbitingTime;
+
     void Start()
     {
-        
+        meshOrignalRotation = platform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float direction = counterClockwise ? -1f : 1f;
+
+        // Rotate the pivot
+        rotationPivot.Rotate(0f, direction * orbitingTime, 0f);
+
+        // Move the Platform mesh in the correct position
+        if (onlyOrbitWithoutRotation)
+        {
+            platform.rotation = meshOrignalRotation;
+        }
     }
 }
