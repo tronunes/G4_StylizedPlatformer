ba323c238d6bda3c54615098915103a9eecb445d
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-03-22 15:40:44 +0200

    Minor improvements to Tongue latching onto targets

--- IndianaFrog/Assets/Scripts/Player/TongueEnd.cs
+++ IndianaFrog/Assets/Scripts/Player/TongueEnd.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class TongueEnd : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
+        // Prevent further collisions
+        gameObject.GetComponent<Collider>().enabled = false;
         gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
 
         // Latch onto the target
-        transform.SetParent(collision.collider.transform);
+        transform.SetParent(collision.collider.transform, true);
+
+        // Orient the tongue to face the surface
+        transform.LookAt(collision.contacts[0].point - collision.contacts[0].normal);
     }
 }
