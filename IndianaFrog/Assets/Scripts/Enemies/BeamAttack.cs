15f00f823598292d1ef3b2a5095e805ef64d7c66
smile32147 <71139107+smile32147@users.noreply.github.com>
2023-03-24 19:38:45 +0200

    Bzzzzt...
    bzzz...

--- IndianaFrog/Assets/Scripts/Enemies/BeamAttack.cs
+++ IndianaFrog/Assets/Scripts/Enemies/BeamAttack.cs
@@ -5,61 +5,70 @@ using UnityEngine;
 public class BeamAttack : MonoBehaviour
 {
 	public Transform firePoint;
+	public LineRenderer lineRenderer;
+	public ParticleSystem impactEffect;
+
 	public LayerMask hitLayer;
-
 	public int damageAmount = 1;
-
-	private LineRenderer lineRenderer;
+	
 	private EnemyTotem enemyTotem;
-
-	private float damageTimer;
 
 
 	private void Start()
 	{
-		lineRenderer = GetComponentInChildren<LineRenderer>();
 		enemyTotem = GetComponentInParent<EnemyTotem>();
 	}
 
 	private void Update()
 	{
-		damageTimer -= Time.deltaTime;
-
 		// if the parent is rotating, only then fire the beam
 		if(enemyTotem.isRotating)
 			ActivateBeam();
 		else
+		{
 			lineRenderer.enabled = false;
+			if(impactEffect.isPlaying)
+				impactEffect.Stop();
+		}
+			
+			
 	}
 
 	private void ActivateBeam()
 	{
 		lineRenderer.enabled = true;
 
-		RaycastHit hit;
-		Debug.DrawRay(firePoint.position, firePoint.forward * enemyTotem.beamLength, Color.red);
-
+		//Debug.DrawRay(firePoint.position, firePoint.forward * enemyTotem.beamLength, Color.red);
 		// if the Raycast hits something, set the end of beam to that point so it will be blocked by obstacles
-		if(Physics.Raycast(firePoint.position, firePoint.forward, out hit, enemyTotem.beamLength, hitLayer))
+		if(Physics.Raycast(firePoint.position, firePoint.forward, out RaycastHit hit, enemyTotem.beamLength, hitLayer))
 		{
 			lineRenderer.SetPosition(0, firePoint.position);
 			lineRenderer.SetPosition(1, hit.point);
 
 			// can also implement particle effect instantiate here
-			//
+			if(!impactEffect.isPlaying)
+				impactEffect.Play();			
+			impactEffect.transform.position = hit.point;
+			impactEffect.transform.rotation = Quaternion.LookRotation(firePoint.position - hit.point);
 
 			// damage the player only once per "interval", otherwise player takes damage
 			// every frame while the beam is passing the player
 			if(hit.transform.CompareTag("Player"))
-			{
-				if(damageTimer <= 0)
 					hit.transform.GetComponent<PlayerHealth>().SubtractHealth(damageAmount);
-			}			
+
 		}
 		else
 		{
 			lineRenderer.SetPosition(0, firePoint.position);
 			lineRenderer.SetPosition(1, firePoint.position + firePoint.forward * enemyTotem.beamLength);
+			//impactEffect.Stop();
 		}
+
+		if(hit.transform == null)
+		{
+			if(impactEffect.isPlaying)
+				impactEffect.Stop();
+		}
+			
 	}
 }
