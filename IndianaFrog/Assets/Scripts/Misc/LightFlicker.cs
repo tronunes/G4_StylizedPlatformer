59ce8e04f8956662c518ffba90896caef15a301a
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-04-04 19:12:44 +0300

    Improved light flicker by moving the light source

--- IndianaFrog/Assets/Scripts/Misc/LightFlicker.cs
+++ IndianaFrog/Assets/Scripts/Misc/LightFlicker.cs
@@ -9,12 +9,17 @@ public class LightFlicker : MonoBehaviour
     [Range(0f, 1f)] public float flickerPercentage;
     public float flickerFrequency = 1;
     private HDAdditionalLightData lightData;
+    private Vector3 lightOriginalPosition;
+    public bool moveLightSource = false; // Moves the lightsource transform position with Perlin noise
+    [Range(0f, 0.2f)] public float lightMovementRadius = 0.05f;
 
 
     void Start()
     {
         lightData = gameObject.GetComponent<HDAdditionalLightData>();;
         originalLightIntensity = lightData.intensity;
+
+        lightOriginalPosition = transform.position;
     }
 
     void Update()
@@ -24,6 +29,18 @@ public class LightFlicker : MonoBehaviour
         {
             float dimmingValue = flickerPercentage * Mathf.PerlinNoise(Time.time * flickerFrequency, 0.0f);
             lightData.intensity = originalLightIntensity - originalLightIntensity * dimmingValue;
+
+            // Change the lightsource position over time with Perlin noise
+            if (moveLightSource)
+            {
+                transform.position =
+                    lightOriginalPosition +
+                        (-Vector3.one + 2f * new Vector3(
+                            Mathf.PerlinNoise(Time.time * flickerFrequency, 10f),
+                            Mathf.PerlinNoise(Time.time * flickerFrequency, 20f),
+                            Mathf.PerlinNoise(Time.time * flickerFrequency, 30f))
+                        ) * lightMovementRadius;
+            }
         }
     }
 }
