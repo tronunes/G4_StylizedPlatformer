4f2eecc338cf8d193ac53c9f0bc5a0562f901729
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-04-06 11:17:09 +0300

    Added the Emblem prefab

---
+++ IndianaFrog/Assets/Scripts/Player/EmblemHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmblemHandler : MonoBehaviour
+{
+    private int emblemsCollected = 0;
+    public int requiredEmblemCount = 0;
+
+
+    void Start()
+    {
+        // If required emblem count is not defined in the inspector
+        // -> count the number of emblems in the scene and require collecting them all
+        if (requiredEmblemCount == 0)
+        {
+            requiredEmblemCount = GameObject.FindGameObjectsWithTag("Emblem").Length;
+        }
+    }
+
+    public void CollectEmblem()
+    {
+        emblemsCollected++;
+        Debug.Log("Emblem collected");
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Emblem"))
+        {
+            CollectEmblem();
+        }
+    }
+}
