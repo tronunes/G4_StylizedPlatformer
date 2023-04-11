ecab94f8598526b9111fe61e63b8800ad58677c8
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-04-11 20:08:21 +0300

    Added UI icons for collected and uncollected emblems

--- IndianaFrog/Assets/Scripts/Player/PlayerEmblemHandler.cs
+++ IndianaFrog/Assets/Scripts/Player/PlayerEmblemHandler.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerEmblemHandler : MonoBehaviour
 {
     private int emblemsCollected = 0;
-    public int requiredEmblemCount = 0;
+    public int requiredEmblemCount = 3;
+
+    [Header("Technical")]
+    [SerializeField] private Transform emblemsUIWrapper;
 
 
     void Start()
     {
         // If required emblem count is not defined in the inspector
         // -> count the number of emblems in the scene and require collecting them all
-        if (requiredEmblemCount == 0)
+        if (requiredEmblemCount == -1)
         {
             requiredEmblemCount = GameObject.FindGameObjectsWithTag("Emblem").Length;
         }
@@ -21,5 +24,18 @@ public class PlayerEmblemHandler : MonoBehaviour
     public void CollectEmblem()
     {
         emblemsCollected++;
+
+        UpdateEmblemsUI();
+    }
+
+    void UpdateEmblemsUI()
+    {
+        // Activate the "Active" icons and disable "Inactive" icons for all collected emblems
+        for (int i = 0; i < emblemsCollected; i++)
+        {
+            Transform emblemIcon = emblemsUIWrapper.Find("Emblem" + (i + 1).ToString());
+            emblemIcon.Find("Active").gameObject.SetActive(true);
+            emblemIcon.Find("Inactive").gameObject.SetActive(false);
+        }
     }
 }
