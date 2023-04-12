70fa047b89e901901f43976cdf6f1fd3c349ac51
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-04-12 15:03:49 +0300

    Use correct emblem part model defined in the scene editor

--- IndianaFrog/Assets/Scripts/Misc/Emblems/Emblem.cs
+++ IndianaFrog/Assets/Scripts/Misc/Emblems/Emblem.cs
@@ -2,8 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum EmblemPartChoices // Choices for the visible part model
+{
+    TOP,
+    RIGHT,
+    BOTTOM
+}
+
 public class Emblem : MonoBehaviour
 {
+    public EmblemPartChoices visiblePartModel;
+    private Transform emblemPartsWrapper;
+
+
+    void Start()
+    {
+        emblemPartsWrapper = transform.Find("Animator").Find("EmblemParts");
+
+        // Hide all part models
+        foreach (Transform child in emblemPartsWrapper)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        // Activate the correct part model
+        switch (visiblePartModel)
+        {
+            case EmblemPartChoices.TOP:
+                emblemPartsWrapper.Find("EmblemPart_Top").gameObject.SetActive(true);
+                break;
+            case EmblemPartChoices.RIGHT:
+                emblemPartsWrapper.Find("EmblemPart_Right").gameObject.SetActive(true);
+                break;
+            case EmblemPartChoices.BOTTOM:
+                emblemPartsWrapper.Find("EmblemPart_Bottom").gameObject.SetActive(true);
+                break;
+        }
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.CompareTag("Player"))
