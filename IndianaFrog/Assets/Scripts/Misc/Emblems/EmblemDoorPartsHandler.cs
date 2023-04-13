c827c7d6a3d0c86d2efee48e1f8b051be15a6932
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-04-13 19:10:21 +0300

    Added particle effects for Emblem insertion

--- IndianaFrog/Assets/Scripts/Misc/Emblems/EmblemDoorPartsHandler.cs
+++ IndianaFrog/Assets/Scripts/Misc/Emblems/EmblemDoorPartsHandler.cs
@@ -6,9 +6,21 @@ using UnityEngine;
 public class EmblemDoorPartsHandler : MonoBehaviour
 {
     public EmblemDoor emblemDoor;
+    public GameObject emblemFuseParticles;
+    public GameObject emblemHoverParticles;
 
     public void StartOpeningDoor()
     {
         emblemDoor.OpenDoor();
+    }
+
+    public void ActivateEmblemFuseParticles()
+    {
+        emblemFuseParticles.SetActive(true);
+    }
+
+    public void ActivateEmblemhoverParticles()
+    {
+        emblemHoverParticles.SetActive(true);
     }
 }
