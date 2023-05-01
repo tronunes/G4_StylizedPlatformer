67ac37b49315d4318edb1f992c3f827c9a018227
Riku Nunes <toni.nunes@metropolia.fi>
2023-05-01 03:50:14 +0300

    -fireball shoot sound

--- IndianaFrog/Assets/Scripts/Enemies/TriggerFire.cs
+++ IndianaFrog/Assets/Scripts/Enemies/TriggerFire.cs
@@ -6,8 +6,15 @@ public class TriggerFire : MonoBehaviour
 {
     [SerializeField] Enemy enemy;
 
+    public AudioSource fireLaunchAudio;
+
     public void TriggerFiring()
     {
         enemy.Fire();
+    }
+
+    public void FireLaunchAudio()
+    {
+        fireLaunchAudio.Play();
     }
 }
