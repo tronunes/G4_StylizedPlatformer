cebd268cec32be1559a0bf2ffc967b5a6973e2fa
Riku Nunes <toni.nunes@metropolia.fi>
2023-05-02 05:08:06 +0300

    -moved things from gamemanager to object in scene -changed files from wav to vorbis

--- IndianaFrog/Assets/Scripts/MusicPlayer.cs
+++ IndianaFrog/Assets/Scripts/MusicPlayer.cs
@@ -7,31 +7,12 @@ public class MusicPlayer : MonoBehaviour
 {
     [Range(0, 1f)]
     public float songVolume = 1f;
-    public AudioSource songMainMenu;
-    public AudioSource songLevel1;
-    public AudioSource songLevel2;
+    public AudioSource song;
     
     void Awake()
     {
-        SetVolume();
-        switch ( SceneManager.GetActiveScene().name )
-        {
-            case "Main Menu":
-                songMainMenu.Play();
-                break;
-            case "Level 1":
-                songLevel1.Play();
-                break;
-            case "Level 2":
-                songLevel2.Play();
-                break;
-        }
+        song.volume = songVolume;
+        song.Play();
     }
 
-    void SetVolume()
-    {
-        songMainMenu.volume = songVolume;
-        songLevel1.volume = songVolume;
-        songLevel2.volume = songVolume;
-    }
 }
